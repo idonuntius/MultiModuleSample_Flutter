61edbfc6df8b0e649215b2bc26208cb49206d936
idonuntius <mc.natsu831@gmail.com>
2021-09-05 13:42:12 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Multi Module Sample Flutter
---
+++ app/android/app/src/main/kotlin/com/idonuntius/multimodulesampleflutter/app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.idonuntius.multimodulesampleflutter.app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
